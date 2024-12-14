3caab62a6e602c01f91a69103279c4bb6088494e
NO_NAME <dylewskidom1@gmail.com>
2024-12-14 16:06:12 +0100

    Minor fixes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, BadRequestException, ConflictException } from '@nestjs/common';
+import { Controller, Post, Body, ConflictException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
