65390aef02126244737cec1f2ba1ec5f086fba48
NO_NAME <dylewskidom1@gmail.com>
2024-12-18 17:04:32 +0100

    Poprawienie logowania

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,8 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
-import { ConflictException } from '@nestjs/common';
+import { ConflictException, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { validate } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+import { Console } from 'console';
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -20,33 +23,96 @@ describe('AuthController', () => {
     expect(controller).toBeDefined();
   });
 
-  it('should successfully register a new user', async () =>
-  {
-    const newUserData: CreateUserDto = {
+  it('should successfully register a new user', async () => {
+    const newUserData = {
       name: 'name',
       lastname: 'lastName',
-      email: 'email',
-      password: 'haslO2452345923582fnw823',
+      email: 'email@gmail.com',
+      password: 'haslO2452345923582fnw823#',
     };
 
-    const res = await controller.register(newUserData);
+    const newUserDataDto = plainToInstance(CreateUserDto, newUserData);
+
+    const errors = await validate(newUserDataDto);
+    expect(errors.length).toBe(0);
+
+    const res = await controller.register(newUserDataDto);
 
     expect(res).toHaveProperty('message', 'User registered successfully');
   });
 
-  it('should throw error if user already exists', async () =>
-  {
-    const newUserData: CreateUserDto = {
+  it('should throw error if user already exists', async () => {
+    const newUserData = {
       name: 'name',
       lastname: 'lastName',
-      email: 'email',
-      password: 'haslO2452345923582fnw823',
+      email: 'email@gmail.com',
+      password: 'haslO2452345923582fnw823#',
     };
 
-    await controller.register(newUserData);
+    const newUserDataDto = plainToInstance(CreateUserDto, newUserData);
 
-    await expect(controller.register(newUserData)).rejects.toThrow(
+    const errors = await validate(newUserDataDto);
+    expect(errors.length).toBe(0);
+
+    await controller.register(newUserDataDto);
+
+    await expect(controller.register(newUserDataDto)).rejects.toThrow(
       new ConflictException('User with this email already exists'),
     );
+  });
+
+  it('should successfully login a existing user', async () => {
+    const newUserData = {
+      name: 'name',
+      lastname: 'lastName',
+      email: 'email@gmail.com',
+      password: 'haslO2452345923582fnw823#',
+    };
+
+    const newUserDataDto = plainToInstance(CreateUserDto, newUserData);
+
+    const errors = await validate(newUserDataDto);
+    expect(errors.length).toBe(0);
+
+    await controller.register(newUserDataDto);
+
+    const res = await controller.login(newUserData.email, newUserData.password);
+
+    console.log(res);
+
+    expect(res).toHaveProperty('message', 'User logged successfully');
+  });
+
+  it('should throw error if user does not exists', async () => {
+    const newUserData = {
+      name: 'name',
+      lastname: 'lastName',
+      email: 'email@gmail.com',
+      password: 'haslO2452345923582fnw823#',
+    };
+
+    await expect(
+      controller.login(newUserData.email, newUserData.password),
+    ).rejects.toThrow(new UnauthorizedException('User does not exist'));
+  });
+
+  it('should throw error if password does not match', async () => {
+    const newUserData = {
+      name: 'name',
+      lastname: 'lastName',
+      email: 'email@gmail.com',
+      password: 'haslO2452345923582fnw823#',
+    };
+
+    const newUserDataDto = plainToInstance(CreateUserDto, newUserData);
+
+    const errors = await validate(newUserDataDto);
+    expect(errors.length).toBe(0);
+
+    await controller.register(newUserDataDto);
+
+    await expect(
+      controller.login(newUserData.email, 'PaslO2452345923582fnw823$'),
+    ).rejects.toThrow(new ConflictException('Bad password'));
   });
 });
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,25 +3,44 @@ import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class AuthService
-{
+export class AuthService {
   public users = [];
 
-  async register(createUserDto: CreateUserDto)
-  {
+  async register(createUserDto: CreateUserDto) {
     const { email, password, name, lastname } = createUserDto;
 
     const userExist = this.users.find((user) => user.email === email);
 
-    if (userExist)
-       {
-        return { message: 'User with this email already exists' };
-      }
+    if (userExist) {
+      return { message: 'User with this email already exists' };
+    }
 
-      const hashedpassword = await bcrypt.hash(password, 12);
+    const hashedpassword = await bcrypt.hash(password, 12);
 
-      this.users.push({id:Date.now().toString(), name:name, lastname:lastname, email:email, password:hashedpassword});
+    this.users.push({
+      id: Date.now().toString(),
+      name: name,
+      lastname: lastname,
+      email: email,
+      password: hashedpassword,
+    });
 
-      return { message: 'User registered successfully' };
+    return { message: 'User registered successfully' };
+  }
+
+  async login(email: string, password: string) {
+    const userExist = this.users.find((user) => user.email === email);
+
+    if (!userExist) {
+      return { message: 'User does not exist' };
+    }
+
+    const valid = await bcrypt.compare(password, userExist.password);
+
+    if (valid) {
+      return { message: 'User logged successfully' };
+    }
+
+    return { message: 'Bad password' };
   }
 }
