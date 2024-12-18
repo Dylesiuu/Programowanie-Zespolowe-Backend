b926ca24356699323132f85ec18b4cbc174197d1
NO_NAME <dylewskidom1@gmail.com>
2024-12-18 17:04:32 +0100

    Improved login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,41 @@
-import { Controller, Post, Body, ConflictException } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
-export class AuthController 
-{
+export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() createUserDto: CreateUserDto)
-  {
+  async register(@Body() createUserDto: CreateUserDto) {
     const res = await this.authService.register(createUserDto);
 
-    if (res.message === 'User with this email already exists')
-    {
-      throw new ConflictException('User with this email already exists');
+    if (res.message === 'User with this email already exists') {
+      throw new ConflictException(res.message);
+    }
+
+    return res;
+  }
+
+  @Post('login')
+  async login(
+    @Body('email') email: string,
+    @Body('password') password: string,
+  ) {
+    const res = await this.authService.login(email, password);
+
+    if (res.message === 'User does not exist') {
+      throw new UnauthorizedException(res.message);
+    }
+
+    if (res.message === 'Bad password') {
+      throw new ConflictException(res.message);
     }
 
     return res;
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import * as bcrypt from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
+import { validate } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -18,17 +20,48 @@ describe('AuthService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should register a new user successfully', async () =>
-  {
-    const newUserData: CreateUserDto = {
+  it('should not throw error with valid data', async () => {
+    const newUserData = {
+      name: 'name',
+      lastname: 'lastName',
+      email: 'email@gmail.com',
+      password: 'haslO2452345923582fnw823#',
+    };
+
+    const newUserDataDto = plainToInstance(CreateUserDto, newUserData);
+
+    const errors = await validate(newUserDataDto);
+    expect(errors.length).toBe(0);
+  });
+
+  it('should throw error with inccorect data', async () => {
+    const newUserData = {
       name: 'name',
       lastname: 'lastName',
       email: 'email',
       password: 'haslO2452345923582fnw823',
     };
 
-    const res = await service.register(newUserData);
+    const newUserDataDto = plainToInstance(CreateUserDto, newUserData);
 
+    const errors = await validate(newUserDataDto);
+    expect(errors.length).not.toBe(0);
+  });
+
+  it('should register a new user successfully', async () => {
+    const newUserData = {
+      name: 'name',
+      lastname: 'lastName',
+      email: 'email@gmail.com',
+      password: 'haslO2452345923582fnw823#',
+    };
+
+    const newUserDataDto = plainToInstance(CreateUserDto, newUserData);
+
+    const errors = await validate(newUserDataDto);
+    expect(errors.length).toBe(0);
+
+    const res = await service.register(newUserDataDto);
 
     expect(res.message).toEqual('User registered successfully');
     expect(service.users[service.users.length - 1]).toHaveProperty(
@@ -43,18 +76,79 @@ describe('AuthService', () => {
     expect(passMatch).toBe(true);
   });
 
-  it('should not allow registration of an already existing user', async () =>
-  {
-    const newUserData: CreateUserDto = {
+  it('should not allow registration of an already existing user', async () => {
+    const newUserData = {
       name: 'name',
       lastname: 'lastName',
-      email: 'email',
-      password: 'haslO2452345923582fnw823',
+      email: 'email@gmail.com',
+      password: 'haslO2452345923582fnw823#',
     };
-    await service.register(newUserData);
 
-    const res = await service.register(newUserData);
+    const newUserDataDto = plainToInstance(CreateUserDto, newUserData);
+
+    const errors = await validate(newUserDataDto);
+    expect(errors.length).toBe(0);
+
+    await service.register(newUserDataDto);
+
+    const res = await service.register(newUserDataDto);
 
     expect(res.message).toEqual('User with this email already exists');
+  });
+
+  it('should login a existing user successfully', async () => {
+    const newUserData = {
+      name: 'name',
+      lastname: 'lastName',
+      email: 'email@gmail.com',
+      password: 'haslO2452345923582fnw823#',
+    };
+
+    const newUserDataDto = plainToInstance(CreateUserDto, newUserData);
+
+    const errors = await validate(newUserDataDto);
+    expect(errors.length).toBe(0);
+
+    await service.register(newUserDataDto);
+
+    const res = await service.login(newUserData.email, newUserData.password);
+
+    expect(res.message).toEqual('User logged successfully');
+  });
+
+  it('should not allow login of a non existing user', async () => {
+    const newUserData = {
+      name: 'name',
+      lastname: 'lastName',
+      email: 'email@gmail.com',
+      password: 'haslO2452345923582fnw823#',
+    };
+
+    const res = await service.login(newUserData.email, newUserData.password);
+
+    expect(res.message).toEqual('User does not exist');
+  });
+
+  it('should not allow login with wrong password', async () => {
+    const newUserData = {
+      name: 'name',
+      lastname: 'lastName',
+      email: 'email@gmail.com',
+      password: 'haslO2452345923582fnw823#',
+    };
+
+    const newUserDataDto = plainToInstance(CreateUserDto, newUserData);
+
+    const errors = await validate(newUserDataDto);
+    expect(errors.length).toBe(0);
+
+    await service.register(newUserDataDto);
+
+    const res = await service.login(
+      newUserData.email,
+      'PaslO24523459282fnw82#',
+    );
+
+    expect(res.message).toEqual('Bad password');
   });
 });
