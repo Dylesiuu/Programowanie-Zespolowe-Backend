002a73ce98cfdf941b9ce441960487ac105da186
Michal <mkrkoko2@gmail.com>
2024-12-20 18:55:10 +0100

    Fixes

--- src/scrolling/scrolling.service.ts
+++ src/scrolling/scrolling.service.ts
@@ -4,10 +4,10 @@ import { error, table } from 'console';
 @Injectable()
 export class ScrollingService {
     private tabela = [
-            {id: 0, name: "Ramen",     age: '1 rok',    discribtion: "pochodzi z warszawy",     gender: 'Pies',     location: "Warszawa",  shelter: "Schronisko na Paluchu",                traids: ["Spokojny", "Czuły", "Leniwy"] , image: 'https://pettownsendvet.com/wp-content/uploads/2023/01/iStock-1052880600-1024x683.jpg'},
-            {id: 1, name: "Pomelo",    age: '2 lata',   discribtion: "pochodzi z torunia",      gender: 'Suka',     location: "Toruń",     shelter: "Schronisko dla zwierząt w Toruniu",    traids: ["Energiczny", "Lojalny", "Ciekawski"], image: 'https://www.rspcasa.org.au/wp-content/uploads/2024/08/Cat-Management-Act-Review-2-768x527.png'},
-            {id: 2, name: "Spongebob", age: '4 lata',   discribtion: "pochodzi z bydgoszczy",   gender: 'Pies',     location: "Bydgoszcz", shelter: "Schronisko dla Zwierząt w Bydgoszczy", treids: ["Przyjacielski", "Energiczny", "Figlarny"], image: 'https://dogshome.com/wp-content/uploads/animalimages//1139184/556697c795ff443c8969ac1c81f9a95a-1728272579-1728272583_other.jpg'},
-            {id: 3, name: "Spongebob", age: '2 lata',   discribtion: "pochodzi z grudziądza",   gender: 'Pies',     location: "Grudziądz", shelter: "Centrum Opieki nad Zwierzętami",       treids: ["Agresywny", "Głośny", "Leniwy"], image: 'https://pettownsendvet.com/wp-content/uploads/2023/01/iStock-1052880600-1024x683.jpg'},
+            {id: 0, name: "Ramen",     age: '1 rok',    discribtion: "pochodzi z warszawy",     gender: 'Pies',     location: "Warszawa",  shelter: "Schronisko na Paluchu",                traits: ["Spokojny", "Czuły", "Leniwy"] , image: 'https://pettownsendvet.com/wp-content/uploads/2023/01/iStock-1052880600-1024x683.jpg'},
+            {id: 1, name: "Pomelo",    age: '2 lata',   discribtion: "pochodzi z torunia",      gender: 'Suka',     location: "Toruń",     shelter: "Schronisko dla zwierząt w Toruniu",    traits: ["Energiczny", "Lojalny", "Ciekawski"], image: 'https://www.rspcasa.org.au/wp-content/uploads/2024/08/Cat-Management-Act-Review-2-768x527.png'},
+            {id: 2, name: "Spongebob", age: '4 lata',   discribtion: "pochodzi z bydgoszczy",   gender: 'Pies',     location: "Bydgoszcz", shelter: "Schronisko dla Zwierząt w Bydgoszczy", traits: ["Przyjacielski", "Energiczny", "Figlarny"], image: 'https://dogshome.com/wp-content/uploads/animalimages//1139184/556697c795ff443c8969ac1c81f9a95a-1728272579-1728272583_other.jpg'},
+            {id: 3, name: "Spongebob", age: '2 lata',   discribtion: "pochodzi z grudziądza",   gender: 'Pies',     location: "Grudziądz", shelter: "Centrum Opieki nad Zwierzętami",       traits: ["Agresywny", "Głośny", "Leniwy"], image: 'https://pettownsendvet.com/wp-content/uploads/2023/01/iStock-1052880600-1024x683.jpg'},
         ];
 
     getPetbyIndex(id: string){
