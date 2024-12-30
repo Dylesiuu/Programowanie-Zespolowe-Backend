67aae4ee53dab79bd16996aa21ee5ff46b86c3cd
NO_NAME <dylewskidom1@gmail.com>
2024-12-30 16:38:53 +0100

    Add database support (untested)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,25 @@ import { HelloController } from './hello/hello.controller';
 import { ScrollingModule } from './scrolling/scrolling.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, ScrollingModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    UserModule,
+    ScrollingModule,
+    AuthModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController, HelloController],
   providers: [AppService],
 })
