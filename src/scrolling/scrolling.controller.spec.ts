a63150c5b1a3c30c37ea748b54581d0a38f326f8
SiwekPiotr <piotr.siwek1209@wp.pl>
2025-01-17 17:41:04 +0100

    Dodawanie zwierząt do bazy danych (jeszcze bez testów)

--- src/scrolling/scrolling.controller.spec.ts
+++ src/scrolling/scrolling.controller.spec.ts
@@ -6,21 +6,21 @@ describe('ScrollingController', () => {
   let controller: ScrollingController;
   let mockService: Partial<ScrollingService>;
 
-  
-
   beforeEach(async () => {
     mockService = {
-      getPetbyIndex: jest.fn(), 
-      getPetbyName: jest.fn(),  
-      getAll: jest.fn()
+      getPetbyIndex: jest.fn(),
+      getPetbyName: jest.fn(),
+      getAll: jest.fn(),
     };
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ScrollingController],
-      providers: [{
-        provide: ScrollingService,
-        useValue: mockService, 
-      },],
+      providers: [
+        {
+          provide: ScrollingService,
+          useValue: mockService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ScrollingController>(ScrollingController);
@@ -31,57 +31,72 @@ describe('ScrollingController', () => {
     expect(controller).toBeDefined();
   });
 
-
   // test 2: zwracanie danych
-  it("return item from table of index 2",()=>{
-      const result = controller.getData("2");
-      expect(mockService.getPetbyIndex).toHaveBeenCalledWith("2"); 
-  })
+  it('return item from table of index 2', () => {
+    const result = controller.getData('2');
+    expect(mockService.getPetbyIndex).toHaveBeenCalledWith('2');
+  });
 
   //test 3: zły indeks(wartosc błedna)
-  it("return rerror for index out of range",()=>{
-    const result = controller.getData("-1");
-    expect(mockService.getPetbyIndex).toHaveBeenCalledWith("-1");
-  })
+  it('return rerror for index out of range', () => {
+    const result = controller.getData('-1');
+    expect(mockService.getPetbyIndex).toHaveBeenCalledWith('-1');
+  });
 
   //test 4: zły indeks(wartosc nie wystepuje)
-  it("return rerror for index out of range",()=>{
-    const result = controller.getData("5");
-    expect(mockService.getPetbyIndex).toHaveBeenCalledWith("5");
-  })
+  it('return rerror for index out of range', () => {
+    const result = controller.getData('5');
+    expect(mockService.getPetbyIndex).toHaveBeenCalledWith('5');
+  });
 
   //test 5: zły indeks(nie liczba)
-  it("return error for index not a number",()=>{
-
-    const result = controller.getData("aa");
-      expect(mockService.getPetbyName).toHaveBeenCalledWith("aa");
-  })
+  it('return error for index not a number', () => {
+    const result = controller.getData('aa');
+    expect(mockService.getPetbyName).toHaveBeenCalledWith('aa');
+  });
 
   //test 6: branie pet z imienia
-  it("return pet by name",()=>{
-    const result = controller.getData("Pomelo");
-    expect(mockService.getPetbyName).toHaveBeenCalledWith("Pomelo");
-  })
+  it('return pet by name', () => {
+    const result = controller.getData('Pomelo');
+    expect(mockService.getPetbyName).toHaveBeenCalledWith('Pomelo');
+  });
 
   //test 7: wiecej niz jeden pet z takim imieniem
-  it("return more than one pet by name",()=>{
-    const result = controller.getData("Spongebob");
-    expect(mockService.getPetbyName).toHaveBeenCalledWith("Spongebob");
-  })
+  it('return more than one pet by name', () => {
+    const result = controller.getData('Spongebob');
+    expect(mockService.getPetbyName).toHaveBeenCalledWith('Spongebob');
+  });
 
   //test 8: zwracanie całej tablicy funkcją getAll
-  it("return table by getAll function",()=>{
+  it('return table by getAll function', () => {
     const mockPetsTable = [
-      {id: 0, name: "Ramen",  age: '1 rok',  discribtion: "pochodzi", gender: 'Pies', location: "Warszawa", shelter: "Schronisko",  traids: ["Spokojny", "Czuły", "Leniwy"] ,       image: '/png'},
-      {id: 1, name: "Pomelo", age: '2 lata', discribtion: "pochodzi",  gender: 'Suka', location: "Toruń",    shelter: "Schronisko", traids: ["Energiczny", "Lojalny", "Ciekawski"], image: '/png'},
+      {
+        id: 0,
+        name: 'Ramen',
+        age: '1 rok',
+        discribtion: 'pochodzi',
+        gender: 'Pies',
+        location: 'Warszawa',
+        shelter: 'Schronisko',
+        traids: ['Spokojny', 'Czuły', 'Leniwy'],
+        image: '/png',
+      },
+      {
+        id: 1,
+        name: 'Pomelo',
+        age: '2 lata',
+        discribtion: 'pochodzi',
+        gender: 'Suka',
+        location: 'Toruń',
+        shelter: 'Schronisko',
+        traids: ['Energiczny', 'Lojalny', 'Ciekawski'],
+        image: '/png',
+      },
     ];
 
     jest.spyOn(mockService, 'getAll').mockReturnValue(mockPetsTable);
 
     const result = controller.gatAllPet();
     expect(result).toEqual(mockPetsTable);
-  })
-
-
-  
+  });
 });
--- src/scrolling/scrolling.service.spec.ts
+++ src/scrolling/scrolling.service.spec.ts
@@ -5,9 +5,9 @@ describe('ScrollingService', () => {
   let service: ScrollingService;
 
   const mockPetsTable = [
-    { id: 1, name: "Pomelo",    age: '2 lata'},
-    { id: 2, name: "Spongebob", age: '4 lata'},
-    { id: 3, name: "Spongebob", age: '2 lata'},
+    { id: 1, name: 'Pomelo', age: '2 lata' },
+    { id: 2, name: 'Spongebob', age: '4 lata' },
+    { id: 3, name: 'Spongebob', age: '2 lata' },
   ];
 
   beforeEach(async () => {
@@ -24,54 +24,50 @@ describe('ScrollingService', () => {
   });
 
   // test 2: zwracanie danych
-  it("return item from table of index 2",()=>{
-    const result = service.getPetbyIndex("2");
+  it('return item from table of index 2', () => {
+    const result = service.getPetbyIndex('2');
     expect(result).toEqual(mockPetsTable[1]);
-  })
+  });
 
   //test 3: zły indeks(wartosc poza zakresem)
-  it("return rerror for index out of range",()=>{
-    const result = service.getPetbyIndex("-1");
-      expect(result).toEqual({ error: 'Index is Invalid.' });
-  })
+  it('return rerror for index out of range', () => {
+    const result = service.getPetbyIndex('-1');
+    expect(result).toEqual({ error: 'Index is Invalid.' });
+  });
 
   //test 4: zły indeks(wartosc poza zakresem)
-  it("return rerror for index not in table",()=>{
-    const result = service.getPetbyIndex("5");
-      expect(result).toEqual({error: 'Pet not found.'});
-  })
+  it('return rerror for index not in table', () => {
+    const result = service.getPetbyIndex('5');
+    expect(result).toEqual({ error: 'Pet not found.' });
+  });
 
   //test 5: zły indeks(nie liczba)
-  it("return error for index not a number",()=>{
-    const result = service.getPetbyIndex("aa");
-      expect(result).toEqual({ error: 'Index is Invalid.' });
-  })
+  it('return error for index not a number', () => {
+    const result = service.getPetbyIndex('aa');
+    expect(result).toEqual({ error: 'Index is Invalid.' });
+  });
 
   // test 6: zwracanie danych z nazwy
-  it("return item from table of name",()=>{
-    const result = service.getPetbyName("Pomelo");
+  it('return item from table of name', () => {
+    const result = service.getPetbyName('Pomelo');
     expect(result).toEqual([mockPetsTable[0]]);
-  })
+  });
 
   //test 7: zła nazwa
-  it("return rerror cause name not found",()=>{
-    const result = service.getPetbyIndex("5");
-      expect(result).toEqual({error: 'Pet not found.'});
-  })
+  it('return rerror cause name not found', () => {
+    const result = service.getPetbyIndex('5');
+    expect(result).toEqual({ error: 'Pet not found.' });
+  });
 
   //test 8: wiecej niz jeden pet z takim imieniem
-  it("return more than one pet by name",()=>{
-    const result = service.getPetbyName("Spongebob");
-      expect(result).toEqual([mockPetsTable[1],mockPetsTable[2]]);
-  })
+  it('return more than one pet by name', () => {
+    const result = service.getPetbyName('Spongebob');
+    expect(result).toEqual([mockPetsTable[1], mockPetsTable[2]]);
+  });
 
-   //test 9: zwracanie całej tablicy funkcją getAll
-   it("return table by getAll function",()=>{
+  //test 9: zwracanie całej tablicy funkcją getAll
+  it('return table by getAll function', () => {
     const result = service.getAll();
-      expect(result).toEqual(mockPetsTable);
-  })
-
-
-
-
+    expect(result).toEqual(mockPetsTable);
+  });
 });
