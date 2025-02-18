663cebc374e177c8cce24a48efc8017d724194b5
NO_NAME <dylewskidom1@gmail.com>
2025-02-18 22:03:43 +0100

    Small changes in tests

--- src/animals/animals.controller.spec.ts
+++ src/animals/animals.controller.spec.ts
@@ -1,15 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AnimalsController } from './animals.controller';
+import { AnimalsService } from './animals.service';
 
 describe('AnimalsController', () => {
   let controller: AnimalsController;
+  let service: AnimalsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AnimalsController],
+      providers: [
+        {
+          provide: AnimalsService,
+          useValue: {
+            // Mock implementation of the service methods
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            create: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AnimalsController>(AnimalsController);
+    service = module.get<AnimalsService>(AnimalsService);
   });
 
   it('should be defined', () => {
