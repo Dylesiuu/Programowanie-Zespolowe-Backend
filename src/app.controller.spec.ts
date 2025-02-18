137a49890a6500bd11d2defc3d5140de82630a44
NO_NAME <dylewskidom1@gmail.com>
2025-02-18 22:03:43 +0100

    Small changes in tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getTitle()).toBe('Hello World!');
+    it('should return "Lecyh"', () => {
+      expect(appController.getTitle()).toBe('Lecyh');
     });
   });
 });
