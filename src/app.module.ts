45fa6f5c54393121909caa4c3d8a93542ffffb78
NO_NAME <dylewskidom1@gmail.com>
2025-02-19 14:21:59 +0100

    Removed HelloWorld module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HelloController } from './hello/hello.controller';
 import { UserModule } from './user/user.module';
 import { ScrollingModule } from './scrolling/scrolling.module';
 import { AuthModule } from './auth/auth.module';
@@ -28,7 +27,7 @@ import mongoose from 'mongoose';
     }),
     AnimalsModule,
   ],
-  controllers: [AppController, HelloController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {
