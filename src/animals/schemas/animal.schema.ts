4324b44d36f69ddb0fc82a243b74ad98e4f0f79a
NO_NAME <dylewskidom1@gmail.com>
2025-02-20 19:19:09 +0100

    Add animals module that was previously temporarily removed

--- src/animals/schemas/animal.schema.ts
+++ src/animals/schemas/animal.schema.ts
@@ -5,8 +5,29 @@ export type AnimalDocument = Animal & Document;
 
 @Schema()
 export class Animal {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true })
+  age: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true })
+  gender: string;
+
+  @Prop({ required: true })
+  location: string;
+
+  @Prop({ required: true })
+  shelter: string;
+
   @Prop({ type: [String], required: true })
-  traits: number[];
+  traits: string[];
+
+  @Prop({ required: true })
+  image: string;
 }
 
 export const AnimalSchema = SchemaFactory.createForClass(Animal);
