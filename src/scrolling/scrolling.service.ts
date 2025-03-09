02443b54776c95d35a6ce1969f82f531ab0f1d29
NO_NAME <dylewskidom1@gmail.com>
2025-03-09 14:08:58 +0100

    Add function that matches animals to the user (without tests)

--- src/scrolling/scrolling.service.ts
+++ src/scrolling/scrolling.service.ts
@@ -1,41 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { error, table } from 'console';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { Pet } from './schema/pet.schema';
+import { matchAnimals } from 'src/utils/matchAnimals';
 
 @Injectable()
 export class ScrollingService {
-    constructor(
-        @InjectModel('Pet') private readonly petModel: Model<Pet>,
-    ) {}
+  constructor(@InjectModel('Pet') private readonly petModel: Model<Pet>) {}
 
+  async getPetbyIndex(id: string): Promise<Pet | { error: string }> {
+    const index = parseInt(id);
+    if (isNaN(index) || index < 0) {
+      return { error: 'Index is Invalid.' };
+    }
+    const result = await this.petModel.findOne({ id: index }).exec();
+    if (!result) {
+      return { error: 'Pet not found.' };
+    }
+    return result;
+  }
 
-    async getPetbyIndex(id: string): Promise<Pet | { error: string }> {
-        const index = parseInt(id);
-        if (isNaN(index) || index < 0 ) {
-            return { error: 'Index is Invalid.' };
-          }
-        const result = await this.petModel.findOne({ id: index }).exec();
-          if(!result){
-            return {error: 'Pet not found.'}
-          }
-        return result;
-        
+  async getAll(): Promise<Pet[]> {
+    return this.petModel.find().exec();
+  }
+
+  async getPetbyName(name: string): Promise<Pet[] | { error: string }> {
+    const matchingPets = await this.petModel.find({
+      name: new RegExp(`^${name}$`, 'i'),
+    });
+
+    if (matchingPets.length === 0) {
+      return { error: 'Pet with that name not found.' };
     }
 
-    async getAll(): Promise<Pet[]> {
-        return this.petModel.find().exec();
-    }
-        
-    async getPetbyName(name: string): Promise<Pet[] | { error: string }> {
-        const matchingPets = await this.petModel.find({ name: new RegExp(`^${name}$`, 'i') });
+    return matchingPets;
+  }
 
-        if (matchingPets.length === 0) {
-            return { error: 'Pet with that name not found.' };
-        }
-
-        return matchingPets;
+  async match(userId: ObjectId): Promise<Pet[] | { message: string }> {
+    const allAnimals = this.getAll();
+    const user = await this.petModel.findById(userId).exec();
+    if (user) {
+      return matchAnimals(user, allAnimals);
     }
 
+    return { message: 'No pets found.' };
+  }
 }
