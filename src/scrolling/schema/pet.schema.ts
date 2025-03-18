2e63895a9410ac236fb009900af26da331fc4314
NO_NAME <dylewskidom1@gmail.com>
2025-03-19 00:32:48 +0100

    Add Swagger decorators for scrolling controller

--- src/scrolling/schema/pet.schema.ts
+++ src/scrolling/schema/pet.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, Schema as MongooseSchema} from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { Document, Schema as MongooseSchema } from 'mongoose';
 import * as mongoose from 'mongoose';
 import * as AutoIncrementFactory from 'mongoose-sequence';
 
@@ -7,29 +8,62 @@ const AutoIncrement = AutoIncrementFactory(mongoose);
 
 @Schema()
 export class Pet extends Document {
+  @ApiProperty({
+    example: 'Pomelo',
+    description: 'The name of the pet',
+  })
   @Prop({ required: true })
   name: string;
 
+  @ApiProperty({
+    example: '2 lata',
+    description: 'The age of the pet',
+  })
   @Prop({ required: true })
   age: string;
 
+  @ApiProperty({
+    example: 'pochodzi z torunia',
+    description: 'The description of the pet',
+  })
   @Prop({ required: true })
   discribtion: string;
 
+  @ApiProperty({
+    example: 'Suka',
+    description: 'The gender of the pet',
+  })
   @Prop({ required: true })
   gender: string;
 
+  @ApiProperty({
+    example: 'Toruń',
+    description: 'The location of the pet',
+  })
   @Prop({ required: true })
   location: string;
 
+  @ApiProperty({
+    example: 'Schronisko dla zwierząt w Toruniu',
+    description: 'The shelter where the animal is',
+  })
   @Prop({ required: true })
   shelter: string;
 
+  @ApiProperty({
+    example: ['64d5a9e8f1b2c24d55a9e8f1'],
+    description: 'List of trait IDs associated with the pet',
+    type: [String],
+  })
   @Prop({
     type: [{ type: MongooseSchema.Types.ObjectId, ref: 'AnimalTrait' }],
   })
   traits: MongooseSchema.Types.ObjectId[];
 
+  @ApiProperty({
+    example: 'https://example.com/pet-image.jpg',
+    description: "The URL of the pet's image",
+  })
   @Prop({ required: true })
   image: string;
 }
--- src/scrolling/scrolling.controller.ts
+++ src/scrolling/scrolling.controller.ts
@@ -2,7 +2,10 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ScrollingService } from './scrolling.service';
 import { ObjectId } from 'mongodb';
 import mongoose from 'mongoose';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Pet } from './schema/pet.schema';
 
+@ApiTags('ScrollingController')
 @Controller('scrolling')
 export class ScrollingController {
   private scrollingService;
@@ -12,6 +15,57 @@ export class ScrollingController {
   }
 
   @Get(':arg')
+  @ApiOperation({
+    description:
+      'This endpoint allows you to retrieve data about a specific pet. ' +
+      'You can search for a pet by providing either the pet ID or the pet name. ' +
+      'If the pet is found, the endpoint will return the pet. ' +
+      'information, otherwise it will return an error message.',
+    summary: 'Return an pet by id or name',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Pet found.',
+    type: Pet,
+    example: {
+      _id: new ObjectId('72f1a2b3c4d5e6f7a8b9c0d3'),
+      name: 'Pomelo',
+      age: '2 lata',
+      discribtion: 'pochodzi z torunia',
+      gender: 'Suka',
+      location: 'Toruń',
+      shelter: 'Schronisko dla zwierząt w Toruniu',
+      traits: [new ObjectId('63e4d5a7f1a2b3c4d5e6f7a8')],
+      image:
+        'https://www.rspcasa.org.au/wp-content/uploads/2024/08/Cat-Management-Act-Review-2-768x527.png',
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'No pet found.',
+    schema: {
+      type: 'object',
+      properties: {
+        error: {
+          type: 'string',
+          example: 'Pet not found.',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid input.',
+    schema: {
+      type: 'object',
+      properties: {
+        error: {
+          type: 'string',
+          example: 'Invalid input.',
+        },
+      },
+    },
+  })
   getData(@Param('arg') arg: string) {
     const isObjectId = (value: string): boolean => {
       return mongoose.isValidObjectId(value);
@@ -24,11 +78,179 @@ export class ScrollingController {
   }
 
   @Get('')
+  @ApiOperation({
+    description:
+      'This endpoint allows you to retrieve data about all pets. ' +
+      'If there are pets in the database, the endpoint will return an array of pets. ' +
+      'If there are no pets in the database, the endpoint will return an empty array.',
+    summary: 'Return all pets',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Pets found.',
+    type: [Pet],
+    example: [
+      {
+        _id: new ObjectId('72f1a2b3c4d5e6f7a8b9c0d3'),
+        name: 'Pomelo',
+        age: '2 lata',
+        discribtion: 'pochodzi z torunia',
+        gender: 'Suka',
+        location: 'Toruń',
+        shelter: 'Schronisko dla zwierząt w Toruniu',
+        traits: [new ObjectId('63e4d5a7f1a2b3c4d5e6f7a8')],
+        image:
+          'https://www.rspcasa.org.au/wp-content/uploads/2024/08/Cat-Management-Act-Review-2-768x527.png',
+      },
+      {
+        _id: new ObjectId('63e4d5a7f1a2b3c4d5e6f7b9'),
+        name: 'Spongebob',
+        age: '4 lata',
+        discribtion: 'pochodzi z bydgoszczy',
+        gender: 'Pies',
+        location: 'Bydgoszcz',
+        shelter: 'Schronisko dla Zwierząt w Bydgoszczy',
+        traits: [
+          new ObjectId('507f1f77bcf86cd799439011'),
+          new ObjectId('4e4d5a7f1a2b3c4d5e6f7a89'),
+        ],
+        image:
+          'https://dogshome.com/wp-content/uploads/animalimages//1139184/556697c795ff443c8969ac1c81f9a95a-1728272579-1728272583_other.jpg',
+      },
+    ],
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'No pets found.',
+    schema: {
+      type: 'array',
+      items: {
+        type: 'object',
+      },
+    },
+    example: [],
+  })
   gatAllPet() {
     return this.scrollingService.getAll();
   }
 
   @Post('match')
+  @ApiOperation({
+    description:
+      'This endpoint allows you to match a user with a pet. ' +
+      'The user must provide their ID, longitude, and latitude. ' +
+      'If the user is found, the endpoint will return a list of pets that match the user. ' +
+      'If the user is not found or no pets are found, the endpoint will return an error message. ',
+    summary: 'Match a user with a pet',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Pets found.',
+    schema: {
+      type: 'object',
+      properties: {
+        message: { type: 'string', example: 'Matched animals' },
+        matchedAnimals: {
+          type: 'array',
+          items: {
+            type: 'object',
+          },
+          example: [
+            {
+              _id: new ObjectId('48a1b2c3d4e5f6a7b8c9d0e2'),
+              name: 'Spongebob',
+              age: '1 rok',
+              discribtion: 'pochodzi z warszawy',
+              gender: 'Pies',
+              location: 'Warszawa',
+              shelter: 'Schronisko na Paluchu',
+              traits: [
+                {
+                  _id: new ObjectId('507f1f77bcf86cd799439011'),
+                  name: 'Trait 1',
+                  priority: 1,
+                },
+                {
+                  _id: new ObjectId('5f50c31b1c9d440000a1b2c3'),
+                  name: 'Trait 2',
+                  priority: 2,
+                },
+              ],
+              image:
+                'https://pettownsendvet.com/wp-content/uploads/2023/01/iStock-1052880600-1024x683.jpg',
+            },
+            {
+              _id: new ObjectId('72f1a2b3c4d5e6f7a8b9c0d3'),
+              name: 'Pomelo',
+              age: '2 lata',
+              discribtion: 'pochodzi z torunia',
+              gender: 'Suka',
+              location: 'Toruń',
+              shelter: 'Schronisko dla zwierząt w Toruniu',
+              traits: [
+                {
+                  _id: new ObjectId('63e4d5a7f1a2b3c4d5e6f7a8'),
+                  name: 'Trait 3',
+                  priority: 3,
+                },
+              ],
+              image:
+                'https://www.rspcasa.org.au/wp-content/uploads/2024/08/Cat-Management-Act-Review-2-768x527.png',
+            },
+          ],
+        },
+        userWithTraits: {
+          type: 'object',
+          example: {
+            _id: new ObjectId('65f4c8e9f0a5a4d3b4a54321'),
+            name: 'Piotr',
+            lastname: 'Wiśniewski',
+            email: 'piotr.wisniewski@example.com',
+            password: 'mypassword789',
+            favourites: [0, 1, 2],
+            traits: [
+              {
+                _id: new ObjectId('55e4d5a7f1a2b3c4d5e6f7a8'),
+                name: 'Trait 2',
+                animalTraits: [
+                  new ObjectId('63e4d5a7f1a2b3c4d5e6f7a8'),
+                  new ObjectId('60c72b2f9b1d8e4a5f6e7d8c'),
+                  new ObjectId('65a1b2c3d4e5f6a7b8c9d0e1'),
+                ],
+              },
+              {
+                _id: new ObjectId('70c72b2f9b1d8e4a5f6e7d8c'),
+                name: 'Trait 3',
+                animalTraits: [new ObjectId('55a4d5a7f1a2b3c4d5e6f7a8')],
+              },
+            ],
+          },
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Possible error responses',
+    content: {
+      'application/json': {
+        examples: {
+          userNotFound: {
+            summary: 'User not found',
+            value: {
+              message: 'user not found',
+            },
+          },
+          petsNotFound: {
+            summary: 'Pets not found',
+            value: {
+              message: 'pets not found',
+            },
+          },
+        },
+      },
+    },
+  })
   async match(
     @Body('userId') userId: string,
     @Body('lng') lng: number,
