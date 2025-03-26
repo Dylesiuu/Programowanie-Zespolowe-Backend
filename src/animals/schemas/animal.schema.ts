b4d1e7a2432a75d6446e4717e9f02d3a95750ba1
SiwekPiotr <piotr.siwek1209@wp.pl>
2025-03-26 20:09:35 +0100

    slight changes with shelter and traits prop

--- src/animals/schemas/animal.schema.ts
+++ src/animals/schemas/animal.schema.ts
@@ -1,35 +1,80 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, ObjectId, Schema as MongoSchema } from 'mongoose';
+import { Schema as MongooseSchema } from 'mongoose';
+//import { ApiProperty } from '@nestjs/swagger';
 
 export type AnimalDocument = Animal & Document;
 
 @Schema()
 export class Animal {
+  /*@ApiProperty({
+    example: 'Lili',
+    description: 'The name of the animal',
+  })*/
   @Prop({ required: true })
   name: string;
 
+  /*@ApiProperty({
+    example: 2020,
+    description: 'Year of birth',
+  })*/
   @Prop({ required: true })
   birthYear: number;
 
+  /*@ApiProperty({
+    example: 6,
+    description: 'Month of birth (1-12)',
+    minimum: 1,
+    maximum: 12,
+  })*/
   @Prop({ required: true })
   birthMonth: number;
 
+  /*@ApiProperty({
+    example: 'Znaleziona na drodze, bardzo przyjazna. W schronisku jest od ...',
+    description: 'A description of the animal',
+  })*/
   @Prop({ required: true })
   description: string;
 
+  /*@ApiProperty({
+    example: 'Kotka',
+    description: 'Gender of the animal',
+  })*/
   @Prop({ required: true })
   gender: string;
 
-  //0 -> dog, 1 -> cat
+  /*@ApiProperty({
+    example: true,
+    description: 'Animal type: false -> Dog, true -> Cat',
+  })*/
   @Prop({ required: true })
   type: boolean;
 
-  @Prop({ type: MongoSchema.Types.ObjectId, required: true })
-  shelter: ObjectId;
+  /*@ApiProperty({
+    example: '60af8845e13b1c002b1a1b45',
+    description: 'Reference to the shelter (ObjectId)',
+  })*/
+  @Prop({ type: MongooseSchema.Types.ObjectId, required: true })
+  shelter: MongooseSchema.Types.ObjectId;
 
-  @Prop({ type: [MongoSchema.Types.ObjectId], required: true })
-  traits: ObjectId[];
+  /*@ApiProperty({
+    example: ['60af8845e13b1c002b1a1b46', '60af8845e13b1c002b1a1b47'],
+    description: 'List of traits IDs (ObjectId)',
+  })*/
+  @Prop({
+    type: [MongooseSchema.Types.ObjectId],
+    required: true,
+    ref: 'AnimalTrait',
+  })
+  traits: MongooseSchema.Types.ObjectId[];
 
+  /*@ApiProperty({
+    example: [
+      'https://example.com/image1.jpg',
+      'https://example.com/image2.jpg',
+    ],
+    description: 'List of images URLs',
+  })*/
   @Prop({ type: [String], required: true })
   images: string[];
 }
