6a2f86740055c7d3c0d6e80a0e4ca56714b4146c
NO_NAME <dylewskidom1@gmail.com>
2025-03-26 11:04:39 +0100

    Add new Swagger decorator for jwt-roles.guard

---
+++ src/decorators/api-roles.decorator.ts
@@ -0,0 +1,35 @@
+import { applyDecorators, SetMetadata } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiForbiddenResponse,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+
+export const ApiRoles = (...roles: string[]) => {
+  return applyDecorators(
+    SetMetadata('roles', roles),
+    ApiBearerAuth(),
+    ApiForbiddenResponse({
+      description: `Required roles: **${roles}** or higher`, //${roles.join(', ')}
+      schema: {
+        type: 'object',
+        properties: {
+          message: { type: 'string', example: 'Lack of required roles' },
+          error: { type: 'string', example: 'Forbidden' },
+          statusCode: { type: 'number', example: 403 },
+        },
+      },
+    }),
+    ApiUnauthorizedResponse({
+      description: 'User is not authorized.',
+      schema: {
+        type: 'object',
+        properties: {
+          message: { type: 'string', example: 'Invalid token' },
+          error: { type: 'string', example: 'Unauthorized' },
+          statusCode: { type: 'number', example: 401 },
+        },
+      },
+    }),
+  );
+};
