c60ec5fe73424acaa23fb794e9897f80d66e7c79
NO_NAME <dylewskidom1@gmail.com>
2025-04-18 14:03:05 +0200

    Add fields to animalTrait

--- src/traits/schemas/animalTrait.schema.ts
+++ src/traits/schemas/animalTrait.schema.ts
@@ -1,15 +1,25 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
 
 export type AnimalTraitDocument = AnimalTrait & Document;
 
 @Schema()
 export class AnimalTrait {
   @Prop({ required: true })
-  name: string;
+  text: string;
 
   @Prop({ required: true })
   priority: number;
+
+  @Prop({ required: true })
+  collectionId: number;
+
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    ref: 'AnimalTrait',
+    required: true,
+  })
+  conflicts: MongooseSchema.Types.ObjectId;
 }
 
 export const AnimalTraitSchema = SchemaFactory.createForClass(AnimalTrait);
