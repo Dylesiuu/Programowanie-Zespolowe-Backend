706ed71ca9d732c1bcd1e69dfed9399429d30e08
NO_NAME <dylewskidom1@gmail.com>
2025-04-18 14:04:30 +0200

    Add fields to userTrait

--- src/traits/schemas/userTrait.schema.ts
+++ src/traits/schemas/userTrait.schema.ts
@@ -6,13 +6,23 @@ export type UserTraitDocument = UserTrait & Document;
 @Schema()
 export class UserTrait {
   @Prop({ required: true })
-  name: string;
+  text: string;
 
   @Prop({
     type: [{ type: MongooseSchema.Types.ObjectId, ref: 'AnimalTrait' }],
     default: [],
   })
   animalTraits: MongooseSchema.Types.ObjectId[];
+
+  @Prop({ required: true })
+  collectionId: number;
+
+  @Prop({
+    type: MongooseSchema.Types.ObjectId,
+    ref: 'AnimalTrait',
+    required: true,
+  })
+  conflicts: MongooseSchema.Types.ObjectId;
 }
 
 export const UserTraitSchema = SchemaFactory.createForClass(UserTrait);
